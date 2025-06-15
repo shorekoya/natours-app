32804e708c198d5f9808b374f7e6ce1ffbbf09b1
Oluwashorefunmi Koya <shorekoya@gmail.com>
2025-06-15 19:42:24 +0100

    Prepare for deployment

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const viewRouter = require('./routes/viewRoutes');
 
 // Start express app
 const app = express();
+app.set('trust proxy', 1);
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(
       Date.now() +
-        process.env.JWT_COOKIE_EXPIRES_IN *
+        Number(process.env.JWT_COOKIE_EXPIRES_IN) *
           24 *
           60 *
           60 *
@@ -26,9 +26,11 @@ const createSendToken = (user, statusCode, res) => {
     ),
     httpOnly: true,
   };
-  if (process.env.NODE_ENV === 'production')
-    cookieOptions.secure = true;
 
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+    cookieOptions.sameSite = 'None'; // Helps with cross-site cookies
+  }
   res.cookie('jwt', token, cookieOptions);
 
   // Remove password from output
