f6b5a5bf452c3503f5f3910f2e28dd03e5ae4b26
Oluwashorefunmi Koya <shorekoya@gmail.com>
2025-06-16 17:26:59 +0100

    Improved Stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -84,7 +84,7 @@ const createBookingCheckout = async (session) => {
   const user = await User.findOne({
     email: session.customer_email,
   }).id;
-  const price = session.display_items[0].unit_amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
